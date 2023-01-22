20f6c9163ff5900a5122d66b1b42f5e8a8b7f79a
profitsw2000 <profitsw2000@gmail.com>
2023-01-22 12:12:55 +0300

    Сделан макет для экрана с описанием фильма.

---
+++ app/src/main/java/profitsw2000/diffapps/presentation/view/fragments/DetailsFragment.kt
@@ -0,0 +1,32 @@
+package profitsw2000.diffapps.presentation.view.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import profitsw2000.diffapps.R
+
+class DetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DetailsFragment()
+    }
+
+    private lateinit var viewModel: DetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/profitsw2000/diffapps/presentation/view/fragments/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package profitsw2000.diffapps.presentation.view.fragments
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
