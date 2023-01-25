c94a374e0e9345bfe236386b7af59035b8fc69b2
profitsw2000 <profitsw2000@gmail.com>
2023-01-25 23:14:41 +0300

    Добавлены фрагменты для основных окон приложения, сделана нижняя навигация.

--- app/src/main/java/profitsw2000/diffapps/MainActivity.kt
+++ app/src/main/java/profitsw2000/diffapps/MainActivity.kt
@@ -2,10 +2,41 @@ package profitsw2000.diffapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import profitsw2000.diffapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragment_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.layout.fragment_home, R.layout.fragment_classes, R.layout.fragment_note, R.layout.fragment_favorite)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+/*    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }*/
 }
