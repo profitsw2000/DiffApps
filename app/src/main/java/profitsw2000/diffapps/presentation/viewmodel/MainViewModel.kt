a4077ba5e8d0038849252990e7023e0d8ba4fd2e
profitsw2000 <profitsw2000@gmail.com>
2023-02-04 15:03:02 +0300

    Создана вью модель MainViewModel.kt с источником постраничных данных LiveData.

---
+++ app/src/main/java/profitsw2000/diffapps/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,28 @@
+package profitsw2000.diffapps.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import profitsw2000.diffapps.data.web.RepositoryWebImpl
+import profitsw2000.diffapps.domain.Repository
+import profitsw2000.diffapps.mappers.RedditPostsMapper
+import profitsw2000.diffapps.model.Post
+
+class MainViewModel(
+    private val repository: Repository,
+    private val redditPostsMapper: RedditPostsMapper
+) : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    private val _postPagedList: LiveData<PagedList<Post>> by lazy {
+        (repository as RepositoryWebImpl).getRedditPostsPagedList(compositeDisposable, redditPostsMapper)
+    }
+    val postPagedList: LiveData<PagedList<Post>> by this::_postPagedList
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+}
